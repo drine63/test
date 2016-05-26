dd6a504ec0a1a0506ba76d3d505ca5af67ecf46a
drine <drine_med@yahoo.fr>
2016-05-26 11:15:20 +0200

    change hostname

--- app.php
+++ app.php
@@ -1,4 +1,4 @@
 <?php
 // this is the front controller
-use Symfony\Component\Controller\Controller
-
+use Symfony\Component\Controller\Controller;
+use Symfony\Component\Httpfundation\Request;
--- vendor/config.php
+++ vendor/config.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFundation\Request;
+use Symfony\Component\HttpFundation\Response;
 use Symfony\Validator\constraints\Validator as Assert;
 
 class ArticleController extends Contorller
